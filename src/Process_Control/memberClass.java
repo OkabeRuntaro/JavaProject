a74e7b3a4bc195d624b0cd2412b386b3c9b14c0b
OkabeRuntaro <992536005@qq.com>
2023-10-02 14:46:44 +0800

    完善了IF判断条件

--- src/Process_Control/memberClass.java
+++ src/Process_Control/memberClass.java
@@ -6,16 +6,26 @@ public class memberClass {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("请输入会员积分：");
-        int score = sc.nextInt();
 
-        if (score>=8000){
-            System.out.println("该会员享受的折扣为：0.6");
-        } else if (score>=4000&&score<8000) {
-            System.out.println("该会员享受的折扣为：0.7");
-        } else if (score>=2000&&score<4000) {
-            System.out.println("该会员享受的折扣为：0.8");
+        //判断小数点
+        if (sc.hasNextInt()){
+            int score = sc.nextInt();
+        //判断正负
+            if (score>0){
+                if (score>=8000){
+                    System.out.println("该会员享受的折扣为：0.6");
+                } else if (score>=4000&&score<8000) {
+                    System.out.println("该会员享受的折扣为：0.7");
+                } else if (score>=2000&&score<4000) {
+                    System.out.println("该会员享受的折扣为：0.8");
+                }else {
+                    System.out.println("该会员享受的折扣为：0.9");
+                }
+            }else {
+                System.out.println("非法数据-");
+            }
         }else {
-            System.out.println("该会员享受的折扣为：0.9");
+            System.out.println("非法数据.");
         }
     }
 }
