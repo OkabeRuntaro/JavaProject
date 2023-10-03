59e8d0fc44ba41d48296cbd7f40412f272df2736
OkabeRuntaro <992536005@qq.com>
2023-10-03 10:49:20 +0800

    更新了循环

---
+++ src/Process_Control/switchClass.java
@@ -0,0 +1,20 @@
+package Process_Control;
+
+public class switchClass {
+    public static void main(String[] args) {
+
+        int score = 97;
+
+        switch (score/10){
+            case 10 :
+                System.out.println("A级");
+                break;
+            case 9 :
+                System.out.println("B级");
+                break;
+            default:
+                System.out.println("C级");
+                //break;
+        }
+    }
+}
